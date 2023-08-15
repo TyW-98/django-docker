2d5e9ffc0cc9b550a2689417d79d3617527b908a
wey1998 <yitwey1998@hotmail.com>
2023-08-15 23:54:29 +0100

    Fix code linting

--- app/app/tests/test_add.py
+++ app/app/tests/test_add.py
@@ -1,42 +1,36 @@
-""" 
+"""
 Sample test
 """
-    
 from django.test import SimpleTestCase
 
 from app import calc
 
 
 class AddTests(SimpleTestCase):
-    """AddTests 
+    """AddTests
 
     Test for calc module
     """
-    
     def test_add_number(self):
-        """test_add_number 
-
+        """test_add_number
         Test sum of number
         """
-        
-        result = calc.add(5,6)
-        
-        self.assertEqual(result,11)
-        
+        result = calc.add(5, 6)
+        self.assertEqual(result, 11)
+
     def test_minus_number(self):
-        """test_minus_number 
+        """test_minus_number
 
         Test minus of number
         """
-        result = calc.minus(10,4)
-        
+        result = calc.minus(10, 4)
         self.assertEqual(result, 6)
-        
+
     def test_multiple_number(self):
-        """test_multiple_number 
+        """test_multiple_number
 
         Test Multiplication of number
         """
-        result = calc.multiply(10,4)
-        
-        self.assertEqual(result,40)
+        result = calc.multiply(10, 4)
+
+        self.assertEqual(result, 40)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Django command to wait for database to full load before running app services
 """
 import time
@@ -10,20 +10,23 @@ from psycopg2 import OperationalError as Psycopg2Error
 
 class Command(BaseCommand):
     """Django Command to wait for database to be ready"""
-    
+
     def handle(self, *args, **kwargs):
         """Entry point for command"""
-        
+
         # Message to display on console as command is executed
         self.stdout.write("Waiting for database...")
         db_up = False
-        
-        while db_up == False: 
-            try: 
-                self.check(databases=['default']) # Will throw exception (Psycopg2Error or OperationalError) if not ready 
+
+        while db_up is False:
+            try:
+                # Will throw exception Psycopg2Error or OperationalError if not ready # noqa:
+                self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write(self.style.WARNING("Database still unavailabel, waitin 1 second before trying..."))
+                self.stdout.write(self.style.WARNING(
+                    "Database still unavailable, waitin 1 second before trying..." # noqa
+                ))
                 time.sleep(1)
-        
+
         self.stdout.write(self.style.SUCCESS("Database is ready!"))
