640919a9e6879ad854ace2e9e05fc372f995bfb3
wey1998 <yitwey1998@hotmail.com>
2023-08-16 16:57:03 +0100

    Add custom base user

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,38 @@
-from django.db import models  # noqa
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    "Manager for custom user"
+    
+    def create_user(self, email, password=None, **kwargs):
+        "Create and save new user."
+        # email = email.split("@")[0] + "@" + email.split("@")[1].lower()
+        if not email: 
+            raise ValueError("Email field is empty")
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self,email, password=None, **kwargs):
+        return self.create_user(email,password, is_staff = True, is_superUser = True, **kwargs)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """Custom User"""
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superUser = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+ 
+    USERNAME_FIELD = "email"
+
---
+++ app/core/tests/test_customUser_models.py
@@ -0,0 +1,40 @@
+"""
+Test Custom user model
+"""
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class UserModelTest(TestCase):
+    """Test Case"""
+
+    def test_create_user_with_email(self):
+        """Test creating user with email"""
+        email = "test@example.com"
+        password = "testpassword"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_normalised_emaiL(self):
+        """Test normalisation of new user's email"""
+        sample_emails = [
+            ["email1@EXAMPLE.com", "email1@example.com"],
+            ["Email2@Example.com", "Email2@example.com"],
+            ["EMAIL3@EXAmple.com", "EMAIL3@example.com"],
+            ["email4@example.COM", "email4@example.com"]
+        ]
+
+        for test_email, expected_email in sample_emails:
+            user = get_user_model().objects.create_user(test_email,"samplepassword")
+
+            self.assertEqual(user.email, expected_email)
+            
+    def test_new_user_empty_email_error(self):
+        """Raise Error if no email is passed in"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "testpassword")
