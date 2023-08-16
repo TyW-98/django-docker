22f74d063d2f60efc628aedf3dedc7e67e8a9611
wey1998 <yitwey1998@hotmail.com>
2023-08-16 22:46:05 +0100

    Change user details display at Custom user admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
 """
 Django Admin Dashboard Customisation
 """
+from core import models
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    """Define custom user admin page layout""" # noqa
+    ordering = ["id"]
+    list_display = ["email", "first_name", "last_name", "is_active", "is_staff"]
+    
+admin.site.register(models.CustomUser, CustomUserAdmin)
