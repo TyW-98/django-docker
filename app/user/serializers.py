7be3ba30c386d067d21dd0a58b83165ea44736b0
wey1998 <yitwey1998@hotmail.com>
2023-08-17 13:22:32 +0100

    Add API Route to allow users to register

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -8,12 +8,12 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializers for user"""
-    
+
     class Meta: 
         model = get_user_model()
         fields = ["email", "password", "first_name", "last_name"]
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
-        
-    def create_user(self, validated_data):
+
+    def create(self, validated_data):
         """Create and return new user with encrpyted password"""
         return get_user_model().objects.create_user(**validated_data)
