5fc376f6adf26c0d6c823840dc093b588a750469
wey1998 <yitwey1998@hotmail.com>
2023-08-19 01:47:35 +0100

    fix code linting

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Tests for Recipe API
 """
 
@@ -15,11 +15,12 @@ from rest_framework.test import APIClient
 RECIPE_URL = reverse("recipe:recipe-list")
 USER_RECIPE_URL = reverse("recipe:user-recipe")
 
-def create_recipe(user, **params): 
+
+def create_recipe(user, **params):
     """Create and return a recipe for testing"""
     default_recipe = {
-        "title":"Sample Recipe",
-        "time_needed":60,
+        "title": "Sample Recipe",
+        "time_needed": 60,
         "cost": Decimal(5.99),
         "description": "Test Recipe Description",
         "link": "http:example.com/test_recipe.pdf/"
@@ -28,22 +29,23 @@ def create_recipe(user, **params):
     default_recipe.update(params)
     return Recipe.objects.create(user, **default_recipe)
 
+
 class PublicRecipeAPITest(TestCase):
     """Test unauthenticated Recipe API request"""
-    
+
     def setUp(self):
         self.client = APIClient()
-    
+
     def test_list_recipe(self):
         """Test successfully fetch API without authentication"""
         res = self.client.get(RECIPE_URL)
-        
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        
+
 
 class PrivateRecipeAPITest(TestCase):
     """Test recipe API requests that requires authentication"""
-    
+
     def setUp(self):
         """Setup user and APIClient which will be used in each test"""
         test_user_details = {
@@ -55,19 +57,19 @@ class PrivateRecipeAPITest(TestCase):
         self.user = get_user_model().objects.create_user(**test_user_details)
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
-        
+
     def test_retrieve_recipes(self):
         """Test retrieving recipes"""
         create_recipe(self.user)
         create_recipe(self.user)
-        
+
         res = self.client.get(RECIPE_URL)
         # Retrieving all recipes
         recipes = Recipe.objects.all().order_by("id")
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer)
-        
+
     def test_retrieving_user_recipes(self):
         """Test retrieving user's recipes"""
         second_test_user_details = {
@@ -76,14 +78,15 @@ class PrivateRecipeAPITest(TestCase):
             "first_name": "first 2",
             "last_name": "last 2"
         }
-        test_user2 = get_user_model().objects.create_user(**second_test_user_details)
+        test_user2 = get_user_model().objects.create_user(
+            **second_test_user_details
+        )
         create_recipe(self.user)
         create_recipe(test_user2)
-        
+
         res = self.client.get(USER_RECIPE_URL)
         # Retrieving user specific recipes
-        recipes = Recipe.objects.filter(user = self.user)
+        recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer)
-        
